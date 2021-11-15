db84d5658edc1de4352c2b7716922c3cc124fba1
Dysaido <tonyyoni@gmail.com>
2021-11-15 14:51:00 +0100

    Modified command register/unregister

--- src/main/java/xyz/dysaido/onevsonegame/OneVSOneGame.java
+++ src/main/java/xyz/dysaido/onevsonegame/OneVSOneGame.java
@@ -1,6 +1,7 @@
 package xyz.dysaido.onevsonegame;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
 import org.bukkit.command.SimpleCommandMap;
 import org.bukkit.plugin.java.JavaPlugin;
 import xyz.dysaido.onevsonegame.command.impl.EventCommand;
@@ -11,14 +12,17 @@ import xyz.dysaido.onevsonegame.util.FileManager;
 import xyz.dysaido.onevsonegame.util.Reflection;
 
 import java.lang.reflect.Field;
+import java.util.Map;
 
 public final class OneVSOneGame extends JavaPlugin {
 
     private static OneVSOneGame instance;
     private SimpleCommandMap simpleCommandMap;
+    private Map<String, Command> knownCommands;
     private FileManager ringConfig;
     private RingManager ringManager;
     private MatchManager matchManager;
+    private EventCommand eventCommand;
 
     public static OneVSOneGame getInstance() {
         return instance;
@@ -32,17 +36,25 @@ public final class OneVSOneGame extends JavaPlugin {
         saveConfig();
         ringManager = new RingManager(ringConfig);
         matchManager = new MatchManager(this);
-
-        Bukkit.getScheduler().runTaskLater(this, () -> ringManager.load(), 100);
-        getCommandMap().register("event", new EventCommand(this));
+        eventCommand = new EventCommand(this);
+        Bukkit.getScheduler().runTaskLater(this, () -> ringManager.load(), 200);
         getServer().getPluginManager().registerEvents(new MatchListener(this), this);
+        registerCommand();
+    }
 
+    private void registerCommand() {
+        getCommandMap().register("event", eventCommand);
+    }
+
+    private void unregisterCommand() {
+        knownCommands.values().remove(eventCommand);
+        eventCommand.unregister(simpleCommandMap);
     }
 
     @Override
     public void onDisable() {
         instance = null;
-
+        unregisterCommand();
     }
 
     public SimpleCommandMap getCommandMap() {
@@ -51,6 +63,14 @@ public final class OneVSOneGame extends JavaPlugin {
             simpleCommandMap = Reflection.getObject(getServer(), field, SimpleCommandMap.class);
         }
         return simpleCommandMap;
+    }
+
+    public Map<String, Command> getKnownCommands(SimpleCommandMap simpleCommandMap) {
+        if (knownCommands == null) {
+            Field field = Reflection.getField(SimpleCommandMap.class, "knownCommands");
+            knownCommands = Reflection.getObject(simpleCommandMap, field, Map.class);
+        }
+        return knownCommands;
     }
 
     public RingManager getRingManager() {
