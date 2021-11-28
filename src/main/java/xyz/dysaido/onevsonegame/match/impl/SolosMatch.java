1037b2e93e0b9a661331d17fabe9a22aa3669abb
Dysaido <tonyyoni@gmail.com>
2021-11-28 17:37:07 +0100

    Simplify

--- src/main/java/xyz/dysaido/onevsonegame/match/impl/SolosMatch.java
+++ src/main/java/xyz/dysaido/onevsonegame/match/impl/SolosMatch.java
@@ -1,11 +1,8 @@
 package xyz.dysaido.onevsonegame.match.impl;
 
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import xyz.dysaido.onevsonegame.OneVSOneGame;
 import xyz.dysaido.onevsonegame.event.SoloGameNextRoundEvent;
-import xyz.dysaido.onevsonegame.event.GamePlayerJoinEvent;
-import xyz.dysaido.onevsonegame.event.GamePlayerLeaveEvent;
 import xyz.dysaido.onevsonegame.event.SoloGamePlayerWinEvent;
 import xyz.dysaido.onevsonegame.match.BaseMatch;
 import xyz.dysaido.onevsonegame.match.MatchState;
@@ -16,42 +13,12 @@ import xyz.dysaido.onevsonegame.setting.Settings;
 import xyz.dysaido.onevsonegame.util.Format;
 import xyz.dysaido.onevsonegame.util.Pair;
 
-import java.util.Objects;
 import java.util.Optional;
 
 public class SolosMatch extends BaseMatch {
 
     public SolosMatch(OneVSOneGame plugin, Ring ring) {
         super("SoloMatch", plugin, ring);
-    }
-
-    @Override
-    public void join(Player player) {
-        Objects.requireNonNull(player);
-        if (waiting > 0) {
-            if (!queue.contains(player)) {
-                MatchPlayer matchPlayer = new MatchPlayer(this, player);
-                Bukkit.getServer().getPluginManager().callEvent(new GamePlayerJoinEvent(this, matchPlayer));
-                queue.addMatchPlayer(matchPlayer);
-                Format.broadcast(Settings.JOIN_MESSAGE.replace("{player}", player.getName()));
-            } else {
-                player.sendMessage(Format.colored(Settings.ALREADY_JOINED_MESSAGE));
-            }
-        } else {
-            player.sendMessage(Format.colored(Settings.EVENT_NOT_AVAILABLE_MESSAGE));
-        }
-    }
-
-    @Override
-    public void leave(Player player) {
-        Objects.requireNonNull(player);
-        if (queue.contains(player)) {
-            MatchPlayer matchPlayer = queue.findByPlayer(player);
-            Bukkit.getServer().getPluginManager().callEvent(new GamePlayerLeaveEvent(this, matchPlayer));
-            matchPlayer.reset(ring.getWorldSpawn(), PlayerState.SPECTATOR);
-            queue.removeMatchPlayer(matchPlayer);
-            Format.broadcast(Settings.LEAVE_MESSAGE.replace("{player}", player.getName()));
-        }
     }
 
     @Override
@@ -114,7 +81,7 @@ public class SolosMatch extends BaseMatch {
     public void nextRound() {
         this.round++;
         Format.broadcast(Settings.NEXT_ROUND.replace("{round}", String.valueOf(round)));
-        Pair<MatchPlayer, MatchPlayer> opponents = this.queue.randomizedOpponents();
+        Pair<MatchPlayer, MatchPlayer> opponents = this.queue.randomizedSolosOpponents();
         Bukkit.getServer().getPluginManager().callEvent(new SoloGameNextRoundEvent(this, opponents));
         MatchPlayer damager = opponents.getKey();
         MatchPlayer victim = opponents.getValue();
