52450df6b2632ce1cfb92b366cb6800f0ecc2a60
Dysaido <tonyyoni@gmail.com>
2021-11-28 17:36:27 +0100

    Join and Leave method was made not abstract

--- src/main/java/xyz/dysaido/onevsonegame/match/BaseMatch.java
+++ src/main/java/xyz/dysaido/onevsonegame/match/BaseMatch.java
@@ -1,15 +1,23 @@
 package xyz.dysaido.onevsonegame.match;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import xyz.dysaido.onevsonegame.OneVSOneGame;
+import xyz.dysaido.onevsonegame.event.GamePlayerJoinEvent;
+import xyz.dysaido.onevsonegame.event.GamePlayerLeaveEvent;
+import xyz.dysaido.onevsonegame.match.model.MatchPlayer;
 import xyz.dysaido.onevsonegame.match.model.PlayerState;
 import xyz.dysaido.onevsonegame.ring.Ring;
 import xyz.dysaido.onevsonegame.setting.Settings;
+import xyz.dysaido.onevsonegame.util.Format;
+
+import java.util.Objects;
 
 public abstract class BaseMatch extends MatchTask {
 
     protected final Ring ring;
     protected final MatchQueue queue;
+    protected final MatchType type;
     protected volatile MatchState state = MatchState.WAITING;
     protected int round = 0;
 
@@ -22,6 +30,7 @@ public abstract class BaseMatch extends MatchTask {
     public BaseMatch(String name, OneVSOneGame plugin, Ring ring) {
         super(name, plugin);
         this.ring = ring;
+        this.type = ring.getMatchType();
         this.queue = new MatchQueue(this);
     }
 
@@ -34,9 +43,32 @@ public abstract class BaseMatch extends MatchTask {
         }
     }
 
-    public abstract void join(Player player);
+    public void join(Player player) {
+        Objects.requireNonNull(player);
+        if (waiting > 0) {
+            if (!queue.contains(player)) {
+                MatchPlayer matchPlayer = new MatchPlayer(this, player);
+                Bukkit.getServer().getPluginManager().callEvent(new GamePlayerJoinEvent(this, matchPlayer));
+                queue.addMatchPlayer(matchPlayer);
+                Format.broadcast(Settings.JOIN_MESSAGE.replace("{player}", player.getName()));
+            } else {
+                player.sendMessage(Format.colored(Settings.ALREADY_JOINED_MESSAGE));
+            }
+        } else {
+            player.sendMessage(Format.colored(Settings.EVENT_NOT_AVAILABLE_MESSAGE));
+        }
+    }
 
-    public abstract void leave(Player player);
+    public void leave(Player player) {
+        Objects.requireNonNull(player);
+        if (queue.contains(player)) {
+            MatchPlayer matchPlayer = queue.findByPlayer(player);
+            Bukkit.getServer().getPluginManager().callEvent(new GamePlayerLeaveEvent(this, matchPlayer));
+            matchPlayer.reset(ring.getWorldSpawn(), PlayerState.SPECTATOR);
+            queue.removeMatchPlayer(matchPlayer);
+            Format.broadcast(Settings.LEAVE_MESSAGE.replace("{player}", player.getName()));
+        }
+    }
 
     public abstract void loop();
 
@@ -64,5 +96,9 @@ public abstract class BaseMatch extends MatchTask {
 
     public void setState(MatchState state) {
         this.state = state;
+    }
+
+    public MatchType getType() {
+        return type;
     }
 }
