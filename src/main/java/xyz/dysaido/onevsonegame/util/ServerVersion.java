6475f488304bfa50fe0e115434ca34817c9159ce
Dysaido <tonyyoni@gmail.com>
2021-12-07 14:31:08 +0100

    Add 1.18 version

--- src/main/java/xyz/dysaido/onevsonegame/util/ServerVersion.java
+++ src/main/java/xyz/dysaido/onevsonegame/util/ServerVersion.java
@@ -17,7 +17,8 @@ public enum ServerVersion {
     v1_16_R1,
     v1_16_R2,
     v1_16_R3,
-    v1_17_R1;
+    v1_17_R1,
+    v1_18_R1;
 
     public boolean before(ServerVersion serverVersion) {
         return serverVersion.ordinal() > this.ordinal();
