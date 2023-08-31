6a090f6588e85d20f547ce0c67a08ad003094597
Dysaido <tonyyoni@gmail.com>
2023-08-31 17:36:09 +0200

    Add metrics

--- src/main/java/xyz/dysaido/pvpevent/PvPEventLoader.java
+++ src/main/java/xyz/dysaido/pvpevent/PvPEventLoader.java
@@ -1,5 +1,7 @@
 package xyz.dysaido.pvpevent;
 
+import org.bstats.bukkit.Metrics;
+import org.bstats.charts.SimplePie;
 import org.bukkit.plugin.java.JavaPlugin;
 import xyz.dysaido.pvpevent.api.PvPEvent;
 import xyz.dysaido.pvpevent.util.Logger;
@@ -8,11 +10,15 @@ public final class PvPEventLoader extends JavaPlugin {
 
     private static final String TAG= "Loader";
     private PvPEventPlugin event;
+    private Metrics metrics;
 
     @Override
     public void onEnable() {
         if (this.event != null) {
             Logger.debug(TAG, String.format("Call enable method from %s", PvPEventPlugin.class));
+            metrics = new Metrics(this, 19693);
+
+            // Optional: Add custom charts
             this.event.enable();
         } else {
             Logger.error(TAG, "PvPEvent was unloaded by unknown reason, please restart the server or use plugman!");
@@ -22,8 +28,14 @@ public final class PvPEventLoader extends JavaPlugin {
     @Override
     public void onDisable() {
         Logger.debug(TAG, String.format("Call disable method from %s", PvPEventPlugin.class));
-        this.event.disable();
-        this.event = null;
+        if (this.event != null) {
+            this.event.disable();
+            this.event = null;
+        }
+        if (this.metrics != null) {
+            this.metrics.shutdown();
+        }
+
     }
 
     @Override
