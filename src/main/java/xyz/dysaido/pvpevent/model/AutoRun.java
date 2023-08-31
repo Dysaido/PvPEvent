b791cdb76fc889a3934858e502bcb745c083e6e0
Dysaido <tonyyoni@gmail.com>
2023-08-31 17:38:08 +0200

    Add autoset manager, pojo, serializer

--- src/main/java/xyz/dysaido/pvpevent/model/AutoRun.java
+++ src/main/java/xyz/dysaido/pvpevent/model/AutoRun.java
@@ -1,2 +1,51 @@
-package xyz.dysaido.pvpevent.model;public class AutoRun {
+package xyz.dysaido.pvpevent.model;
+
+import java.util.Date;
+
+public class AutoRun {
+
+    private final String name;
+    private String arenaName;
+    private String command;
+    private String broadcast;
+    private Date date;
+    public AutoRun(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setBroadcast(String broadcast) {
+        this.broadcast = broadcast;
+    }
+
+    public String getBroadcast() {
+        return broadcast;
+    }
+
+    public void setDate(long date) {
+        this.date = new Date(date);
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getArenaName() {
+        return arenaName;
+    }
+
+    public void setArenaName(String arenaName) {
+        this.arenaName = arenaName;
+    }
 }
--- src/main/java/xyz/dysaido/pvpevent/serializer/AutosetSerializer.java
+++ src/main/java/xyz/dysaido/pvpevent/serializer/AutosetSerializer.java
@@ -1,2 +1,85 @@
-package xyz.dysaido.pvpevent.serializer;public class AutosetSerializer {
+package xyz.dysaido.pvpevent.serializer;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import xyz.dysaido.pvpevent.model.AutoRun;
+import xyz.dysaido.pvpevent.model.manager.AutosetManager;
+import xyz.dysaido.pvpevent.util.Logger;
+import xyz.dysaido.pvpevent.util.YamlStorage;
+
+import java.util.*;
+import java.util.logging.Level;
+import java.util.stream.Stream;
+
+public class AutosetSerializer {
+    private static final String TAG = "AutosetSerializer";
+    private final AutosetManager autosetManager;
+    private final YamlStorage storage;
+    public AutosetSerializer(AutosetManager autosetManager, YamlStorage storage) {
+        this.autosetManager = autosetManager;
+        this.storage = storage;
+    }
+
+    public List<AutoRun> read() {
+        List<AutoRun> runs = new ArrayList<>();
+        FileConfiguration conf = storage.getFile();
+        for (String sectionName : conf.getKeys(false)) {
+            ConfigurationSection section = conf.getConfigurationSection(sectionName);
+            try {
+                AutoRun run = new AutoRun(sectionName);
+                run.setArenaName(section.getString("arena"));
+                run.setBroadcast(section.getString("broadcast"));
+                run.setCommand(section.getString("command"));
+                run.setDate(section.getLong("date"));
+                runs.add(run);
+            } catch (Exception e) {
+                Bukkit.getLogger().log(Level.WARNING, "Error:", e);
+                Logger.warning(TAG, String.format("Cannot read %s from autosets.yml. Suppose to delete this section from yaml file!", sectionName));
+            }
+        }
+        return runs;
+    }
+
+    public void append(AutoRun run) {
+        FileConfiguration conf = storage.getFile();
+        writePojo(conf, run);
+
+        storage.saveFile();
+    }
+
+    public void remove(String identifier) {
+        FileConfiguration conf = storage.getFile();
+        if (conf.isConfigurationSection(identifier)) {
+            conf.set(identifier, null);
+            this.storage.saveFile();
+        }
+    }
+
+    public void write() {
+        FileConfiguration conf = storage.getFile();
+        stream().forEach(run -> writePojo(conf, run));
+
+        this.storage.saveFile();
+    }
+
+    private void writePojo(FileConfiguration conf, AutoRun run) {
+        ConfigurationSection section = storage.getFile().getConfigurationSection(run.getName());
+        if (section == null) {
+            section = conf.createSection(run.getName());
+        }
+
+        section.set("arena", run.getName());
+        section.set("broadcast", run.getBroadcast());
+        section.set("command", run.getCommand());
+        section.set("date", run.getDate().toString());
+    }
+
+    protected Stream<AutoRun> stream() {
+        return autosetManager.getAll().values().stream().sorted(Comparator.comparing(AutoRun::getName));
+    }
+
+    public YamlStorage getStorage() {
+        return storage;
+    }
 }
